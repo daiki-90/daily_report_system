dd6e537fa876a8486125b05a1a77c8f495844a78
daiki.kikuchi <uaprkhvdlm.72@icloud.com>
2020-11-17 16:48:27 +0900

    Add Target index

---
+++ src/controllers/targets/TargetEmployeesIndex.java
@@ -0,0 +1,75 @@
+package controllers.targets;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Employee;
+import models.Report;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class TargetEmployeesIndex
+ */
+@WebServlet("/target/index")
+public class TargetEmployeesIndex extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TargetEmployeesIndex() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        Employee target_employee =  em.find(Employee.class, Integer.parseInt(request.getParameter("id")));
+
+        int page;
+        try{
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch(Exception e) {
+            page = 1;
+        }
+
+        List<Report> reports = em.createNamedQuery("getMyAllReports", Report.class)
+                .setParameter("employee", target_employee)
+                .setFirstResult(15 * (page - 1))
+                .setMaxResults(15)
+                .getResultList();
+
+        long reports_count = (long)em.createNamedQuery("getMyReportsCount", Long.class)
+                .setParameter("employee", target_employee)
+                .getSingleResult();
+
+        em.close();
+
+        request.setAttribute("reports", reports);
+        request.setAttribute("reports_count", reports_count);
+        request.setAttribute("page", page);
+        request.setAttribute("target_employee", target_employee);
+
+        if(request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/target/index.jsp");
+        rd.forward(request, response);
+
+    }
+
+}
